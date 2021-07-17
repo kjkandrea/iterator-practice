6640e4ee20c2b0e45c040b9eae6c495a068e3d42
andrea <kjkandrea@gmail.com>
2021-07-17 19:28:30 +0900

    Refactor generateEl
    우와 이것도 fp가 되네

--- assistant/timestamp/renderer.js
+++ assistant/timestamp/renderer.js
@@ -7,17 +7,17 @@ class Renderer {
     this.target.append(el)
   }
   generatePartialElement(timeStamp) {
-    const liElement = document.createElement('li')
-    Object.entries(timeStamp)
+    return Object.entries(timeStamp)
       .map(([label, time]) => [time, `${label} : ${time}`])
-      .forEach(([data, text]) => {
+      .reduce((elements, [data, text]) => {
         const dataElement = document.createElement('data')
         const textNode = document.createTextNode(text)
         dataElement.setAttribute('value', data.toString())
         dataElement.append(textNode)
-        liElement.append(dataElement)
-      })
-    return liElement
+        elements.append(dataElement)
+        return elements;
+      }, document.createElement('li'))
+
   }
 }
 
