fd0ad63aa3b72a0ebfdc969d1f62d5d19a5ddadb
andrea <kjkandrea@gmail.com>
2021-07-17 18:47:56 +0900

    Add set timeStampStupid way

--- foreach-async.js
+++ foreach-async.js
@@ -1,11 +1,24 @@
 import asyncTimeStamp from '/assistant/asyncTimeStamp.js'
 
 const runner = {
+  range: new Array(100).fill(0).map((_, i) => i),
   init() {
-    this.test()
+    this.forEachAsync.init.call(this)
   },
   test() {
     asyncTimeStamp.get().then(console.log)
+  },
+  forEachAsync: {
+    timeStampList: [],
+    init() {
+      runner.forEachAsync.run(this.range)
+    },
+    run(range) {
+      range.forEach(async () => {
+        const timeStamp = await asyncTimeStamp.get()
+        this.timeStampList.push(timeStamp)
+      })
+    }
   }
 }
 
