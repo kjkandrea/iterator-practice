97bf785847118bf20413b3e01f7e8219d9c68004
andrea <kjkandrea@gmail.com>
2021-07-18 18:20:44 +0900

    Add createLikedIterator

--- iterator-generator.js
+++ iterator-generator.js
@@ -3,8 +3,33 @@ const runner = {
     this.run();
   },
   run() {
-    console.log('run')
+    this.createLikedIterator.init()
   },
+  createLikedIterator: {
+    init() {
+      this.useNext()
+
+      console.log(this.iterator.next().value)
+      console.log(this.iterator.next().done)
+    },
+    iterator : (() => {
+      let num = 1
+
+      return {
+        next: () => {
+          num = num * 2
+          return num > 10
+          ? { done: true }
+          : { done: false, value: num }
+        }
+      }
+    })(),
+    useNext() {
+      console.log(this.iterator.next().value)
+      console.log(this.iterator.next().value)
+      console.log(this.iterator.next().value)
+    }
+  }
 }
 
 window.addEventListener('DOMContentLoaded', runner.init.bind(runner))
